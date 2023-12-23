0ca2694b66d6e374fde4fb856e0bc8ec5627ff38
AimonShakil <aimon.ipas@gmail.com>
2023-12-23 13:49:54 +0500

    new changes

---
+++ Index.ts
@@ -0,0 +1,82 @@
+//Project Calculator
+
+import inquirer from "inquirer"
+import chalk from "chalk";
+import chalkAnimation from 'chalk-animation';
+import { log } from "console";
+
+const sleep = ()=>{
+    return new Promise((res)=>{
+        setTimeout(res, 2000);
+    })
+    
+}
+async function Welcome () {
+    let rainbowTitle = chalkAnimation.rainbow('Lets start Calculation');
+    await sleep ();
+    rainbowTitle.stop();
+    console.log(`
+    _____________________
+   |  _________________  |
+   | | JO           0. | |
+   | |_________________| |
+   |  ___ ___ ___   ___  |
+   | | 7 | 8 | 9 | | + | |
+   | |___|___|___| |___| |
+   | | 4 | 5 | 6 | | - | |
+   | |___|___|___| |___| |
+   | | 1 | 2 | 3 | | x | |
+   | |___|___|___| |___| |
+   | | . | 0 | = | | / | |
+   | |___|___|___| |___| |
+   |_____________________|`)
+}
+
+Welcome();
+
+
+
+let answer = await inquirer.prompt([
+
+   {
+    type: 'number',
+    name: 'num1',
+    message: 'Enter the first number:'
+   },
+   {
+    type: 'number',
+    name: 'num2',
+    message: 'Enter the Second number :'
+   },
+   {
+    type: 'list',
+    name: 'operator',
+    message: 'Select the Operator you want to perform',
+    choices: ['Add', 'Subtract', 'Multiply', 'Divide']
+   }
+]);
+
+let result: number;
+
+switch(answer.operator){
+
+    case 'Add': 
+    result = answer.num1 + answer.num2;
+    console.log("The answer for addition is: " + result);
+    break;
+
+    case 'Subtract': 
+    result = answer.num1 - answer.num2;
+    console.log("The answer for subtraction is: " + result);
+    break;
+
+    case 'Multiplication': 
+    result = answer.num1  *  answer.num2;
+    console.log("The answer for multiplication is: " + result);
+    break;
+
+    case 'Divsion': 
+    result = answer.num1 / answer.num2;
+    console.log("The answer for division is: " + result);
+    break;
+} 
---
+++ Project.js
@@ -0,0 +1,8 @@
+"use strict";
+//Project Calculator
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var x = await inquirer_1.default.prompt([{
+        message: "Please enter first number"
+    }
+]);
